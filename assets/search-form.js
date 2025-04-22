5dcf8b18c9faa371846e0579414f0852ee4bfa7b
urvi-s-eq <urvi.sheth@equestsolutions.net>
2025-04-22 16:34:57 +0530

    dynamic collection page

--- assets/search-form.js
+++ assets/search-form.js
@@ -1,3 +1,15 @@
+function debounce(func, wait) {
+  let timeout;
+  return function (...args) {
+    const later = () => {
+      clearTimeout(timeout);
+      func.apply(this, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+}
+
 class SearchForm extends HTMLElement {
   constructor() {
     super();
